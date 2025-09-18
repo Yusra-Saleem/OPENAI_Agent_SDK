034d19201935b2dd2190e335a3f1b1b71f11b260
Yusra-Saleem <rayyan726.m@gmail.com>
2025-09-18 21:37:22 +0500

    Signed-off-by: Yusra-Saleem <rayyan726.m@gmail.com>

---
+++ context-management.py
@@ -0,0 +1,94 @@
+from dataclasses import dataclass
+from agents import Agent, RunContextWrapper, Runner, function_tool ,OpenAIChatCompletionsModel, AsyncOpenAI, enable_verbose_stdout_logging
+from agents.run import RunConfig
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# client = AsyncOpenAI(
+#     api_key= GEMINI_API_KEY ,
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+# )
+
+# model = OpenAIChatCompletionsModel(
+#     openai_client= client,
+#     model="gemini-1.5-flash",
+# )
+
+# config = RunConfig(
+#     model=model,
+#     tracing_disabled=True,
+# )
+
+
+
+# @dataclass 
+# class UserInfo:
+#     name: str 
+#     uid: str
+
+
+# @function_tool
+# def UserContextInfo( wrapper : RunContextWrapper[UserInfo], )-> str:
+#     """Gets the user's full name and age from their profile."""
+#     return f"User {wrapper.context.name} age is 30 years old"
+
+# def main():
+#     agent = Agent[UserInfo]( 
+#         name="Assistant",
+#         instructions="You should use the tool to get the user's information and provide a complete response including both their name and age.",
+#         tools=[UserContextInfo],
+#     )
+
+
+#     result = Runner.run_sync(agent , "What is my name and age?" , context=UserInfo(name="Alice", uid="12345"), run_config = config  ) 
+
+#     print(result.final_output)
+
+
+# main()
+
+
+
+
+client = AsyncOpenAI(
+    api_key = GEMINI_API_KEY,
+    base_url = "https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    openai_client = client,
+    model = "gemini-1.5-flash",
+)
+
+config = RunConfig(
+    model = model,
+    tracing_disabled=True,
+)
+
+enable_verbose_stdout_logging()
+
+@dataclass
+class UserData:
+    name:str
+    age:int
+
+@function_tool
+def UserInfo(wrapper:RunContextWrapper[UserData])->str :
+    """get the user name and age from their profile"""
+    return f" Her name is {wrapper.context.name} and her age is {wrapper.context.age} years old"
+
+
+agent =  Agent[UserData](
+
+    "Assistant" , 
+    "You should use the tool to get the user's information" ,
+    tools= [UserInfo] ,
+)
+
+result = Runner.run_sync( agent , "what's her age and name?" ,context= UserData(name = "Yusra", age =19), run_config = config)
+
+print(result.final_output)
